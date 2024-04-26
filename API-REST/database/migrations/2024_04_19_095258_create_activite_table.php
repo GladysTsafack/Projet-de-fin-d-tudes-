ac796c365f534cbeb10a186b7bf45d23e1e43c0e
GladysTsafack <tsafackgladys604@example.com>
2024-04-26 15:15:02 +0100

    mise à jour de la base de données

--- API-REST/database/migrations/2024_04_19_095258_create_activite_table.php
+++ API-REST/database/migrations/2024_04_19_095258_create_activite_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Categorie;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +20,7 @@ return new class extends Migration
             $table->time('heure');
             $table->timestamps();
             $table->foreignIdFor(Categorie::class);
+            $table->foreignIdFor(User::class);
         });
     }
 
